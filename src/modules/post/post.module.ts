336729167b383aef84642d66991eea17523169bd
Станислав Зиганшин <stanislavzgn@gmail.com>
2025-04-17 23:22:00 +0300

    fix: like post and delete post

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -5,9 +5,10 @@ import { UserEntity } from '@/modules/user/entities/user.entity';
 import { PostService } from '@/modules/post/post.service';
 import { PostController } from '@/modules/post/post.controller';
 import { PostLikeEntity } from '@/modules/postLike/entities/postLike.entity';
+import { CommentEntity } from '@/modules/comment/entities/comment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity, UserEntity, PostLikeEntity])],
+  imports: [TypeOrmModule.forFeature([CommentEntity, PostEntity, UserEntity, PostLikeEntity])],
   providers: [PostService],
   controllers: [PostController],
 })
