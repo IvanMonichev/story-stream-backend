5dc0beeb4a89d0f758949d51092343011731575c
Станислав Зиганшин <stanislavzgn@gmail.com>
2025-04-18 07:40:17 +0300

    fix: like post

--- src/modules/postLike/entities/postLike.entity.ts
+++ src/modules/postLike/entities/postLike.entity.ts
@@ -1,6 +1,6 @@
 import { PostEntity } from '@/modules/post/entities/post.entity';
 import { UserEntity } from '@/modules/user/entities/user.entity';
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('post_likes')
@@ -11,9 +11,17 @@ export class PostLikeEntity {
 
   @ApiProperty()
   @ManyToOne(() => UserEntity, (user) => user.likes)
+  @JoinColumn({ name: 'user_id' })
   user: UserEntity;
+
+  @Column({ name: 'user_id', nullable: false })
+  userId: number;
 
   @ApiProperty()
   @ManyToOne(() => PostEntity, (post) => post.likes)
+  @JoinColumn({ name: 'post_id' })
   post: PostEntity;
+
+  @Column({ name: 'post_id', nullable: false })
+  postId: number;
 }
