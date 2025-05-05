9c02145535606f5fa28677546a4418989541db56
IvanMonichev <monichevid@baltech.ru>
2025-05-05 10:14:50 +0300

    feat: update method

--- src/modules/load/load.service.ts
+++ src/modules/load/load.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 export class LoadService {
   simulateCpu(): void {
     let result = 0;
-    for (let i = 0; i < 1e9; i++) {
+    for (let i = 0; i < 1e6; i++) {
       result += Math.sin(i) * Math.cos(i) + Math.pow(i, 2);
     }
     console.log(result);
