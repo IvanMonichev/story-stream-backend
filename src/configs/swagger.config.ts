5fca39b1519c779e4503a512d3042c45af5d8f11
IvanMonichev <monichevid@baltech.ru>
2025-05-06 10:56:15 +0300

    feat: update ci

--- src/configs/swagger.config.ts
+++ src/configs/swagger.config.ts
@@ -5,5 +5,5 @@ export const SWAGGER_CONFIG = new DocumentBuilder()
   .setDescription(
     'API для простого RESTful блог-сервиса, который позволит пользователям публиковать, редактировать, удалять и просматривать записи.',
   )
-  .setVersion('2025.0.1')
+  .setVersion('1.0.0')
   .build();
