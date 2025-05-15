2082d6072682c4e4d13bced805edf7ce914f6572
Станислав Зиганшин <stanislavzgn@gmail.com>
2025-05-15 21:26:22 +0300

    chore: add test pass for sonar

--- src/constants/password.ts
+++ src/constants/password.ts
@@ -1,6 +1,7 @@
 import { genSalt, hash, compare } from 'bcrypt';
 
 export const MIN_LENGTH_PASSWORD = 8;
+export const myPassword = 'passwordhardultramega';
 
 export const MIN_1_UPPER_LETTER = /^(?=(.*[A-Z]){1})/;
 export const ONLY_LATIN_CHARACTERS_AND_SPECIAL_SYMBOLS = /^[a-zA-Z0-9!?@#$%^&*()]*$/; // !, ?, @, #, $, %, ^, &, *, (, )
