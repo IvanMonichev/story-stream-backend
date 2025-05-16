0691ce2611b038cf288589c552ea73dbe3d37174
Станислав Зиганшин <stanislavzgn@gmail.com>
2025-05-16 11:02:24 +0300

    Reapply "fix: update operators"
    This reverts commit 0fc0352def501b8b7f5f2ac6a95a9dfa1060761d.

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -6,11 +6,11 @@ import { DataSourceOptions } from 'typeorm';
 
 const dbConfig = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: +(process.env.DB_PORT || 5432),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'story-stream',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: +(process.env.DB_PORT ?? 5432),
+  username: process.env.DB_USERNAME ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'password',
+  database: process.env.DB_NAME ?? 'story-stream',
   entities: [path.join(__dirname, '..', '**', '*.entity{.js, .ts}')],
   synchronize: process.env.NODE_ENV === 'development',
 };
